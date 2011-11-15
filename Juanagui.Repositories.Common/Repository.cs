8a2750d824d453dfed3fcfe6819fb846130598d1
jagui <juan@juanagui.com>
2011-11-15 01:10:30 +0100

    repository abstraction

---
+++ Juanagui.Repositories.Common/Repository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Juanagui.Repositories.Common
+{
+    public abstract class Repository<T> : IDisposable
+        where T : class
+    {
+        public abstract void Dispose();
+        public abstract IQueryable<T> Query();
+        public abstract IEnumerable<T> All();
+        public abstract void Add(T entity);
+        public abstract void Attach(T entity);
+        public abstract void Delete(T entity);
+        public abstract void PersistAll();
+    }
+}
