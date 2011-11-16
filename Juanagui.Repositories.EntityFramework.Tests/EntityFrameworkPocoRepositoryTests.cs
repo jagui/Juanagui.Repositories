5609c3cf247ad0090b2db3d2b18f9fb317b3b6eb
Juan Agui <juan@juanagui.com>
2011-11-16 11:02:22 +0100

    added unit tests

---
+++ Juanagui.Repositories.EntityFramework.Tests/EntityFrameworkPocoRepositoryTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data.Common;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity;
+
+namespace Juanagui.Repositories.EntityFramework.Tests
+{
+    [TestClass]
+    public class EntityFrameworkPocoRepositoryTests
+    {
+        private TestContext _dbContext;
+        private EntityFrameworkPocoRepository<Parent> _repository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _dbContext = new TestContext();
+            new TestContextInitializer().InitializeDatabase(_dbContext);
+            _repository = new EntityFrameworkPocoRepository<Parent>(_dbContext);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            var connectionString = _dbContext.Database.Connection.ConnectionString;
+            _repository.Dispose();
+            Database.Delete(connectionString);
+        }
+
+        [TestMethod]
+        // ReSharper disable InconsistentNaming
+        public void AddNewEntity_RetrieveFromRepository_MatchOnOneProperty()
+        // ReSharper restore InconsistentNaming
+        {
+            var father = new Father() { FirstName = "John", LastName = "Doe", DateOfBirth = DateTime.Today.AddYears(-34) };
+            _repository.Add(father);
+            _repository.PersistAll();
+            Assert.AreEqual(father.FirstName, _repository.All().Single().FirstName);
+        }
+
+        [TestMethod]
+        // ReSharper disable InconsistentNaming
+        public void AddNewEntity_ThenDelete_NoMatches()
+        // ReSharper restore InconsistentNaming
+        {
+
+            Parent father = new Father() { FirstName = "John", LastName = "Doe", DateOfBirth = DateTime.Today.AddYears(-34) };
+            _repository.Add(father);
+            _repository.PersistAll();
+            father = _repository.All().Single();
+            _repository.Delete(father);
+            _repository.PersistAll();
+            Assert.IsFalse(_repository.All().Any());
+        }
+    }
+}
---
+++ Juanagui.Repositories.EntityFramework.Tests/TestsModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace Juanagui.Repositories.EntityFramework.Tests
+{
+    public abstract class Parent
+    {
+        public long Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        private IEnumerable<Child> Children { get; set; }
+    }
+
+    public class Father : Parent
+    {
+    }
+
+    public class Mother : Parent
+    {
+        public String MaidenName { get; set; }
+    }
+
+    public class Child
+    {
+        public long Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        private IEnumerable<Parent> Parents { get; set; }
+    }
+
+    public class TestContext : DbContext
+    {
+        private DbSet<Parent> Parents { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Parent>().HasKey(p => p.Id);
+            modelBuilder.Entity<Child>().HasKey(p => p.Id);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class TestContextInitializer : DropCreateDatabaseAlways<TestContext>
+    {
+        protected override void Seed(TestContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
+
